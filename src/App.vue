de1b73bc709280816d41d3c556eeea1167bbb5ae
BajistaRex <alanilt03@gmail.com>
2024-10-02 17:47:52 +0000

    ⭐ S11 Methods Create method to add item and clena input

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,39 @@
   import { ref } from 'vue';
   // Modelo
   const header = ref('App lista de compras');
+  // ----Items----
+  // Item-Model
   const items = ref([ 
     {id: '0', label: '10 Bolillos'},
     {id: '1', label: '1 Lata Frijoles'},
     {id: '2', label: '1 Chelas'},
     {id: '3', label: '1 Nutella'},
   ]);
+  // Item-Method
+const saveItem = () => {
+  // Add new Item
+  items.value.push({id: items.value.length + 1, label: newItem.value});
+  // Clean the input
+  newItem.value = '';
+};
+
+  const newItem = ref('');
   const newItemHighPriority = ref(false);
+
 </script>
 
 <template>
-  <h1>
-    <i class="material-icons shopping-cart-icon">local_mall</i>
-    {{ header }}
+  <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}
     
   </h1>
   <div class="add-item form">
 <!-- Agrupando en un div las entradas -->
-<form v-on:submit.prevent="items.push({ id: items.length + 1, label: newItem })" class="add-item form">
+<form v-on:submit.prevent="saveItem">
 <!-- entrada de texto -->
 	<input
-  v-model.trim="newItem"
-  type="text"
-  placeholder="Add Item"
+    type="text"
+    placeholder="Add Item"
+    v-model.trim="newItem"
   />
     <!-- Caja de seleccion de prioridad -->
     <label>
