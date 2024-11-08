bc16fb80aafc557570bdc8c70b66ad2564f6767d
BajistaRex <alanilt03@gmail.com>
2024-11-08 16:48:36 +0000

    ⭐ S0304 Componente PlanPicker

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <script setup>
-import { ref } from 'vue';
-import planItem from './components/plan-item.vue';
-const plans= 
-ref(['El cafetero', 'El chelero', 'El intelectual']);
+import PlanPicker from './components/PlanPicker.vue'
 </script>
 
 <template>
   <header>
     <div>
       <span id="logo">Liquid Gold Box</span>
-      <img src="./assets/logo.png" alt="logo">
+      <img src="./assets/logo.png" alt="logo" />
     </div>
   </header>
 
@@ -20,11 +17,36 @@ ref(['El cafetero', 'El chelero', 'El intelectual']);
       Viajamos por el mundo para encontrar el mejor café de origen único para ti
     </h2>
 
-    <div class="plans">
-      <plan-item v-for="plan in plans" :key="plan" :name="plan"> </plan-item>
-    </div>
+    <!-- TODO INCLUDE PLAN-PICKER COMPONENT -->
+    <PlanPicker />
   </div>
 </template>
 
 <style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
 </style>
